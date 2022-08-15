2d62d0b1c9f07c99cdce45a5bd4d78e914d13d58
MariusTanase <pulsemusic8@hotmail.com>
2022-08-15 18:50:38 +0100

    Major Update: Adding Playlists, function when the select changes and changes the audiosrc

--- dist/app.js
+++ dist/app.js
@@ -2,20 +2,76 @@ const backgroundElement = document.querySelector('.bg');
 // JSON data for images
 
 let images = [{
-    "url": "./dist/images/bgImage1.jpg",
-},
-{
-    "url": "./dist/images/bgImage2.jpg",
-}]
+        "url": "./dist/images/bgImage1.jpg",
+    },
+    {
+        "url": "./dist/images/bgImage2.jpg",
+    }
+]
+
+// Selecting values from the options form and storing them in variables, from variables we select the values to be equal to the objects parameters in the json file and then we use the values to play a playlist from soundcloud in the background that can be manipulated by the user in audio player that is made in the html file.
+let playlist = [{
+    genre: "deephouse",
+    src: "http://live.dancemusic.ro:7000/stream?type=http&nocache=339198"
+}, {
+    genre: "ibiza",
+    src: "https://ssl1.viastreaming.net:7005/;listen.mp3"
+}, {
+    genre: "dancehall",
+    src: "http://pulseedm.cdnstream1.com:8124/1373_128"
+}, {
+    genre: "chill",
+    src: "http://95.211.3.65:8805/stream/1/"
+}, {
+    genre: "techno",
+    src: "http://51.89.195.240:8034/stream/1/"
+}];
+
+// selecting the value from options form and storing it in a variable from select
+let select = document.getElementsByTagName('select')[0];
+
+// Update the select value with the value from option
+function updateSelect() {
+    select = document.getElementsByTagName('select')[0].value;
+    changeAudioPlayer(select)
+}
+
+// Event listener in the select element to update the select value
+select.addEventListener("change", updateSelect);
+
+// Change Audio Player src to the value of the select element from the playlist array
+
+
+let audioPlayer = document.querySelector('.player');
+
+// compare the select value with the genre value from the playlist array and if they are equal then change the audio player src to the src value from the playlist array
+function changeAudioPlayer(select) {
+    for (let i = 0; i < playlist.length; i++) {
+        if (select == playlist[i].genre) {
+            audioPlayer.src = playlist[i].src;
+        }
+    }
+}
+
+
+
 
 // rotating background based on images in json
 let currentImage = 0;
-
+// function to rotate images
 function changeBackground() {
     let image = images[currentImage];
+    // set the background image to the image url from the json file
     backgroundElement.style.backgroundImage = `url(${image.url})`;
+    // if the current image is equal to the length of the images array then set the current image to 0
     currentImage = (currentImage + 1) % images.length;
 }
-
+// set the interval to change the background image every 30 seconds - Dev Enviroment
 setInterval(changeBackground, 30000);
+// Initial run of the changeBackground function to set the background image to the first image
 changeBackground();
+
+
+// Audio Elements
+
+
