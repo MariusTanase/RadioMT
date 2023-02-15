0984780b5d558b6531f680685dfbc7df8d6c4600
MariusTanase <pulsemusic8@hotmail.com>
2023-02-15 20:18:26 +0000

    Restructure of the project

---
+++ reactRadio/MTRadioReact/src/App.jsx
@@ -0,0 +1,12 @@
+import Background from "./components/background/background"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Background />
+    </div>
+  )
+}
+
+export default App
---
+++ reactRadio/MTRadioReact/src/components/background/background.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './background.module.css'
+
+
+const Background = () => {
+  return (
+    <div className={styles.background}></div>
+  )
+}
+
+export default Background
