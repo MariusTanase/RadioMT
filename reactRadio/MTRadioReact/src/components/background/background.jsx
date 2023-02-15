a5ec919c96eb3226fa21d88f08d9d2bf17e98b49
MariusTanase <pulsemusic8@hotmail.com>
2023-02-15 20:28:08 +0000

    Removing react strict mode

--- reactRadio/MTRadioReact/src/components/background/background.jsx
+++ reactRadio/MTRadioReact/src/components/background/background.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './background.css';
 
 
@@ -9,25 +9,30 @@ const Background = () => {
     // the images are in an array
     // the array is in the background module
 
+    console.log('background component')
+
     const imagesArray = [
-        require('../../assets/images/bgImage1.jpg'),
-        require('../../assets/images/bgImage2.jpg'),
-        require('../../assets/images/bgImage3.jpg'),
+        '../../assets/images/bgImage1.jpg',
+        '../../assets/images/bgImage2.jpg',
+        '../../assets/images/bgImage3.jpg',
     ]
     const randomImage = imagesArray[Math.floor(Math.random() * imagesArray.length)]
-    const backgroundStyle = {
-        backgroundImage: `url(${randomImage})`
-    }
-    
+
     // the image will be changed to background class
     // the background class is in background.module.css
     // the background class is in the div tag
 
-    document.body.style.backgroundImage = `url(${randomImage})`
 
+    useEffect(() => {
+        let background = document.querySelector('.background');
+        background.style.backgroundImage = `url(${randomImage})`;
+    }, [])
 
   return (
-    <div className='background'></div>
+    <div className='background'>
+
+        <h1>This is BG</h1>
+    </div>
   )
 }
 
