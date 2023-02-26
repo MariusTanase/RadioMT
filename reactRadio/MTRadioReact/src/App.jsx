e79d2b68e69d40494fc9362a82540473c53d8bba
MariusTanase <pulsemusic8@hotmail.com>
2023-02-26 19:05:07 +0000

    Adding the backbone of the player component

--- reactRadio/MTRadioReact/src/App.jsx
+++ reactRadio/MTRadioReact/src/App.jsx
@@ -1,10 +1,12 @@
 import Background from "./components/background/background"
+import Player from "./components/player/Player"
 
 function App() {
 
   return (
     <div className="App">
       <Background settingsInterval={60000} />
+      <Player />
     </div>
   )
 }
--- reactRadio/MTRadioReact/src/components/player/Player.jsx
+++ reactRadio/MTRadioReact/src/components/player/Player.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './Player.css'
 
 const Player = () => {
   return (
-    <div>Player</div>
+    <div className='player__container'>
+        <div className='player__container-title'>
+            <h1>MT Radio</h1>
+        </div>
+        <div className='player__container-player'>
+            <audio controls>
+                <source src="https://streaming.radio.co/sd1b1b1b1b/listen" type="audio/mpeg" />
+            </audio>
+        </div>
+
+    </div>
   )
 }
 
