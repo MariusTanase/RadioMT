39726590399bc5b30d573ba98fdcef0e0b94dabc
MariusTanase <pulsemusic8@hotmail.com>
2023-02-26 19:00:47 +0000

    Adding base files for the player

--- reactRadio/MTRadioReact/src/components/background/background.jsx
+++ reactRadio/MTRadioReact/src/components/background/background.jsx
@@ -27,9 +27,7 @@ const Background = ({settingsInterval}) => {
         }
     }, [])
 
-    const particlesInit = async (main) => {
-        console.log(main);
-    
+    const particlesInit = async (main) => {  
         // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
         // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
         // starting from v2 you can add only the features you need reducing the bundle size
