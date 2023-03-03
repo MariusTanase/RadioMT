b184901fdfe83a4c225dbe11fec225ffc90fdcf2
MariusTanase <pulsemusic8@hotmail.com>
2023-03-03 20:17:39 +0000

    Adding an intro animation to website

--- reactRadio/MTRadioReact/src/App.jsx
+++ reactRadio/MTRadioReact/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
 
   return (
     <div className="App">
+      <Intro />
       <Background/>
       <Player />
     </div>
---
+++ reactRadio/MTRadioReact/src/components/intro/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './intro.css'
+
+
+const intro = () => {
+  return (
+    <div className='intro-animation'></div>
+  )
+}
+
+export default intro
