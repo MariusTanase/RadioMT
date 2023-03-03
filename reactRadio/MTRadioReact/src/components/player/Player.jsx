e1fa687f15b7e11f749ffa47c36ca1a9c6864918
MariusTanase <pulsemusic8@hotmail.com>
2023-03-03 21:18:48 +0000

    Updating the list of radios

--- reactRadio/MTRadioReact/src/components/player/Player.jsx
+++ reactRadio/MTRadioReact/src/components/player/Player.jsx
@@ -25,7 +25,7 @@ const Player = () => {
             id: 2,
             title: 'Classic',
             artist: 'FM',
-            genre: 'Dance',
+            genre: 'Classical',
             image: 'https://uk.radio.net/images/broadcasts/79/5a/121368/1/c300.png',
             url: 'https://media-ssl.musicradio.com/ClassicFM'
         },
@@ -44,7 +44,33 @@ const Player = () => {
             genre: 'Deep House',
             image: 'https://cdn.onlineradiobox.com/img/l/8/12078.v9.png',
             url: 'https://edge.singsingmusic.net/MC2.mp3'
+        },
+        {
+            id: 5,
+            title: 'Capital UK',
+            artist: 'Pop',
+            genre: 'Pop',
+            image: 'https://radio-live-uk.com/assets/image/radio/180/capital-fm.jpg',
+            url: 'https://media-ssl.musicradio.com/CapitalUK'
+        },
+        {
+            id: 6,
+            title: 'Deep House Radio',
+            artist: 'Romania',
+            genre: 'Deep House',
+            image: 'https://cdn.onlineradiobox.com/img/l/4/80154.v2.png',
+            url: 'http://62.210.105.16:7000/;stream/1'
+        },
+        {
+            id: 7,
+            title: 'ZILLION!FM',
+            artist: 'Vocal Deep House Radio',
+            genre: 'Deep House',
+            image: 'https://cdn.onlineradiobox.com/img/l/7/53347.v37.png',
+            url: 'https://ascella.streamerr.co/listen/zillionfm/mobile.mp3'
         }
+        
+
     ]
 
     const audioRef = useRef(null)
