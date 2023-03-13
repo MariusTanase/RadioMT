712e95f9b5dcd4a3b76f5a88b500ccef02c9953c
MariusTanase <pulsemusic8@hotmail.com>
2023-03-13 19:17:04 +0000

    Updated the importa in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import Background from "./components/background/background"
-import Player from "./components/player/Player"
-import Intro from "./components/intro/intro"
-import RadioList from "./components/radiosList/RadioList"
 import Main from "./pages/Main"
 
 function App() {
