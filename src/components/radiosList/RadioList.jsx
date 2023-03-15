42e50d642ac999db5d7bc09d6023b47b3508a8d8
MariusTanase <pulsemusic8@hotmail.com>
2023-03-15 18:50:35 +0000

    Radio selection button finished

--- src/components/radiosList/RadioList.jsx
+++ src/components/radiosList/RadioList.jsx
@@ -3,10 +3,21 @@ import { radios } from '../../radios'
 import './RadioList.css'
 
 const RadioList = () => {
-  const changeRadio = (e) => {
+  const changeRadio = (radio) => {
     // change the source of the audio player
+    const {url, title, genre, image} = radio
+    // console.log(element);
     const audioPlayer = document.querySelector('audio')
-    audioPlayer.src = e.target.src
+    const playerTitle = document.querySelector('.title')
+    const genreTitle = document.querySelector('.artist')
+    const playerImage = document.querySelector('.artwork')
+    
+    playerTitle.innerHTML = title
+    genreTitle.innerHTML = genre
+    playerImage.src = image
+
+    audioPlayer.src = url;
+
     audioPlayer.play()
   }
 
@@ -17,9 +28,9 @@ const RadioList = () => {
         <ul className='radios-list'>
           {/* map through the radios list and create a list item for each radio */}
           {radios.map((radio) => {
-            const { id, title} = radio
+            const { id, title, url, genre} = radio
             return (
-              <li key={id} className='radio-element' onClick={(url) => changeRadio(url)}>
+              <li key={id} className='radio-element' onClick={() => changeRadio(radio)}>
                 <div className='radio-image'>
                   <img src={radio.image} alt={title} />
                 </div>
