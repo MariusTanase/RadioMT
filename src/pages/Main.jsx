b21e9b07f02d80152e2d6ef4bd68ca263119ea5a
MariusTanase <pulsemusic8@hotmail.com>
2023-03-15 19:14:41 +0000

    Correction to component in main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Background from '../components/background/background'
-import Footer from '../components/footer/footer'
+import Footer from '../components/footer/Footer'
 import Player from '../components/player/Player'
 import RadioList from '../components/radiosList/RadioList'
 import { radios } from '../radios'
