5637011d8a264f1e70d71d88334cc396f4c0b873
MariusTanase <pulsemusic8@hotmail.com>
2023-03-15 19:02:18 +0000

    Simple footer element

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const footer = () => {
+  return (
+    <div className='footer'>
+        <p>Created by <a href='https://mariustanase.com'>Marius Tanase</a></p>
+    </div>
+  )
+}
+
+export default footer
