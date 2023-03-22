a24a5d47a7fb4beae8663e5b5de07260b94e5e5f
MariusTanase <pulsemusic8@hotmail.com>
2023-03-22 21:38:24 +0000

    adding setRadioImage function

--- src/lib/setArtist.js
+++ src/lib/setArtist.js
@@ -10,4 +10,9 @@ function setRadioName(radioName) {
     document.querySelector('.title').textContent = radioName;
 }
 
+function setRadioImage(radioImage, radioName, genre) {
+    let image = document.querySelector('.radio-image').src = radioImage;
+    image.alt = `${radioName} - with the genre ${genre} image`
+}
+
 export default {setArtist, setRadio, setRadioName};
