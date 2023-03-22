cc3d7016dbc3bca0fd7f08ad1d873921dd832d6f
MariusTanase <pulsemusic8@hotmail.com>
2023-03-22 21:23:46 +0000

    Default RAFCE components

---
+++ src/components/playerButtons/ShuffleButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShuffleButton = () => {
+  return (
+    <div>ShuffleButton</div>
+  )
+}
+
+export default ShuffleButton
---
+++ src/components/playerButtons/VolumeBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VolumeBar = () => {
+  return (
+    <div>VolumeBar</div>
+  )
+}
+
+export default VolumeBar
