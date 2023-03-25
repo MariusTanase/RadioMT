e794ee86f508f6f442cfbe9e59e7649ab57550c9
MariusTanase <pulsemusic8@hotmail.com>
2023-03-25 15:01:54 +0000

    Basic functionability of settings button

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -6,6 +6,30 @@ import { faGear } from '@fortawesome/free-solid-svg-icons'
 
 
 const Settings = () => {
+
+  const handleMenu = () => {
+    const button = document.querySelector('.settings-button')
+    button.classList.add('hidden')
+  
+    const settingsMenu = document.querySelector('.settings-menu')
+    settingsMenu.classList.remove('hidden')
+  
+    const closeButton = document.querySelector('.settings-button__close')
+    closeButton.classList.remove('hidden')
+  }
+  
+  const closeMenu = () => {
+    const closeButton = document.querySelector('.settings-button__close')
+    closeButton.classList.toggle('hidden')
+  
+    const settingsMenu = document.querySelector('.settings-menu')
+    settingsMenu.classList.add('hidden')
+  
+    const button = document.querySelector('.settings-button')
+    button.classList.remove('hidden')
+  }
+
+
   return (
     <div className='settings-container'>
         {/* button with fa setting that will open and close as a menu */}
@@ -27,10 +51,29 @@ const Settings = () => {
             () => {
                 handleMenu()
             }
-        }>            
+        }>       
             <FontAwesomeIcon icon={faGear} className='spin'/>
         </button>
+        <div className='settings-menu hidden'>
+            <div className='settings-menu-item'>
+                <p>Change Background</p>
+            </div>
+            <div className='settings-menu-item'>
+                <p>Change Theme</p>
+                <div className='settings-menu__item-options'>
+                    <div className='settings-menu-item-option'>
+                        <p>Dark</p>
+                        <p>Crimson</p>
+                        <p>Light</p>
+                    </div>
+                </div>
+            </div>
 
+            <button className='settings-button__close' onClick={
+            () => {
+              closeMenu()
+            }}>Close</button>
+      </div>
     </div>
   )
 }
