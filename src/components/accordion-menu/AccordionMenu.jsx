ae45ac83ace6b20553826badf1f2b0c3294f82b6
MariusTanase <pulsemusic8@hotmail.com>
2023-03-26 16:20:47 +0100

    Custom generation of the menu

--- src/components/accordion-menu/AccordionMenu.jsx
+++ src/components/accordion-menu/AccordionMenu.jsx
@@ -2,19 +2,32 @@ import React from 'react'
 import './AccordionMenu.css'
 
 const AccordionMenu = ({category, content}) => {
+    console.log(content);
+
+    const randomKey = () => {
+        return Math.random() * 999999
+    }
+
+
   return (
         <div>
             <label htmlFor='state'>
                 <h5 className='menu-category'>
-                    Button
+                    {category}
                 </h5>
             </label>
             <input type='checkbox' id='state' hidden/>
             <div className='content'>
                 <div className='inner'>
-                    <p>Content</p>
-                    <p>COntent</p>
-                    <p>Content</p>
+                    {
+                        Object.keys(content).map((item) => {
+                            return (
+                                <div key={randomKey()}>
+                                    <h6 className='menu-item'>{item}</h6>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </div>
