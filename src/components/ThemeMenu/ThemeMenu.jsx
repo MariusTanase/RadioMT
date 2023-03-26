808ae86239333adee36fd2c693f6023c9d0e4b51
MariusTanase <pulsemusic8@hotmail.com>
2023-03-26 17:42:58 +0100

    Attempt to make a theme changer function

--- src/components/ThemeMenu/ThemeMenu.jsx
+++ src/components/ThemeMenu/ThemeMenu.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './ThemeMenu.css'
 
 const ThemeMenu = () => {
+    // save in localstorage the theme selected
+    const handleTheme = (theme) => {
+        localStorage.setItem('theme', theme)
+
+        // set the theme in media prefer color scheme
+        window.matchMedia(`(prefers-color-scheme: ${theme})`);
+    }
+
+    useEffect(() => {
+        // get the theme saved in localstorage
+        const theme = localStorage.getItem('theme')
+        // if there is no theme saved, set the default theme
+        if (!theme) {
+            localStorage.setItem('theme', 'Blue')
+
+            window.matchMedia('(prefers-color-scheme: Blue)');
+        } else {
+            // if there is a theme saved, set the theme
+            window.matchMedia(`(prefers-color-scheme: ${theme})`);
+        }
+    }, [])
 
 
   return (
@@ -11,20 +32,25 @@ const ThemeMenu = () => {
             </h5>
             <div className='theme-container__items'>
                 <div className='theme-item'>
-                    <div className='theme-item__name'>
-                        Dark
-                    </div>
-                    </div>
-                <div className='theme-item'>
-                    <div className='theme-item__name'>
-                        Crimson
-                    </div>
-                </div>
-                <div className='theme-item'>
-                    <div className='theme-item__name'>
-                        Light
+                        <div className='theme-item__name' onClick={() => handleTheme('Dark')}>
+                            Dark
                         </div>
                     </div>
+                <div className='theme-item'>
+                        <div className='theme-item__name' onClick={() => handleTheme('Crimson')}>
+                            Crimson
+                        </div>
+                </div>
+                <div className='theme-item'>
+                        <div className='theme-item__name' onClick={() => handleTheme('Light')}>
+                            Light
+                        </div>
+                </div>
+                <div className='theme-item'>
+                        <div className='theme-item__name' onClick={() => handleTheme('Blue')}>
+                            Blue
+                        </div>
+                </div>
             </div>
         </div>
     )
